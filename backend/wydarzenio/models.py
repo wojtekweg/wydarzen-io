f4c4665a209161eb85129cb376da74a1981ef538
wojtek <ojtek@outlook.com>
2021-11-22 22:11:11 +0100

    changed a bit file field

--- backend/wydarzenio/models.py
+++ backend/wydarzenio/models.py
@@ -18,7 +18,12 @@ class Event(models.Model):
     description = models.TextField(max_length=1024, default="TBA")
     place = models.ForeignKey(Place, on_delete=models.CASCADE, default=1)
     is_cancelled = models.BooleanField(default=False)
-    picture = models.ImageField(upload_to="event/posters", blank=True)
+    picture = models.ImageField(
+        upload_to="event/posters", 
+        blank=True, 
+        null=True,
+        default="http://127.0.0.1:8000/media/event/posters/tumblr_a4f6104f246a9fcd3c46f4212a1f9d9c_be7b3587_1280.png")
+        # TODO change default
 
     def __str__(self) -> str:
         return f"{self.title} ({self.date})"
