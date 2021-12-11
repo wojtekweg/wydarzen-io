29e38ca0bfc7cd48b3d1cef0ad8e44e5d86e10ac
wojtek <ojtek@outlook.com>
2021-12-11 20:01:36 +0100

    moved forward with uploading json objects

--- frontend/src/components/ImportModal.js
+++ frontend/src/components/ImportModal.js
@@ -1,7 +1,6 @@
-// TODO edit it to allow import files
-
 import React, { useState } from "react";
 import axios from "axios";
+import config from "../config.json";
 import {
   Button,
   Modal,
@@ -13,10 +12,43 @@ import {
   Label,
 } from "reactstrap";
 
+// TODO create helpers and cleanup
+const logError = (error) => {
+  if (error.response) {
+    console.error(error.response.data);
+  } else if (error.request) {
+    console.error(error.request);
+  } else {
+    console.error("Error", error.message);
+  }
+};
+
 function CustomModal(props) {
-  // const [toggle, setToggle] = useState(props.toggle)
+  // const [toggle, setToggle] = useState(props.toggle);
+  const [jsonFile, setJsonFile] = useState(null);
+
+  const handleFileUpload = async (file, type = file.type, e) => {
+    // Design pattern - Adapter
+    if (type.includes("json")) {
+      console.log(file);
+      setJsonFile(JSON.parse(file));
+    } else if (type.includes("ics")) {
+      // TODO convert to .json
+    }
+  };
+
+  const handleFileSelect = (evt) => {
+    // TODO parse properly the .json file
+    let reader = new FileReader();
+    // reader.onload = function (e) {
+    //   that.displayData(e.target.result);
+    // };
+    reader.readAsText(evt.target.files[0]);
+    setJsonFile(JSON.parse(JSON.stringify(evt.target.files[0])));
+  };
 
   const postData = () => {
+    axios.post(config.url + "events/", jsonFile).catch(logError);
     return;
   };
 
@@ -38,18 +70,24 @@ function CustomModal(props) {
         </p>
         <Form>
           <FormGroup>
-            <Label for="picture">
+            <Label for="file">
               <b>Calendar file</b>
             </Label>
             <br />
             <input
               type="file"
-              id="picture"
-              label="picture file"
-              // onChange={(e) => handlePictureUpload(e.target.files[0])}
+              id="file"
+              label="calendar file"
+              // onChange={(e) => handleFileUpload(e.target.files[0])}
+              onChange={handleFileSelect}
             />
           </FormGroup>
         </Form>
+        {jsonFile === null ? null : (
+          <span className="code">
+            <p>{JSON.stringify(jsonFile)}</p>
+          </span>
+        )}
       </ModalBody>
       <ModalFooter>
         <Button color="success" onClick={postData}>
