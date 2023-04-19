5148cf2e568cb0f22b5c0895940f5447e6c14311
Wojciech Węgrzyn <49337058+wojtekweg@users.noreply.github.com>
2023-04-19 09:56:06 +0200

    Update About.js

--- frontend/src/components/static-subpages/About.js
+++ frontend/src/components/static-subpages/About.js
@@ -1,17 +1,8 @@
 function About() {
   return (
-    <div className="sm:w-2/3 sm:pr-8 sm:py-8  border-gray-200 sm:border-t-0 border-t mt-4 pt-4 sm:mt-0 text-center sm:text-left dark:text-zinc-200">
+    <div className="w-2/3 pr-8 py-8 border-gray-200 sm:border-t-0 border-t mt-4 pt-4 sm:mt-0 text-center text-left dark:text-zinc-200">
       <div className="leading-relaxed text-lg mb-4 whitespace-pre-line">
-        <h1>About wydarzen.io</h1>
-        <h2>Current features</h2>
-        <ul>
-          <li>Adding an event</li>
-          <ul>
-            Events dashboard
-            <li>Filtering events (frontend)</li>
-            <li>Searching for events (frontend)</li>
-          </ul>
-        </ul>
+        <a href="https://github.com/wojtekweg/wydarzen-io/blob/main/docs/dokumentacja_pl.pdf">https://github.com/wojtekweg/wydarzen-io/blob/main/docs/dokumentacja_pl.pdf</a>
       </div>
     </div>
   );
